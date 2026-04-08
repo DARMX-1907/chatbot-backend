951a421893af28033853bdb0f623a1b7b1797c0f
DARMX-1907 <darmagede86@gmail.com>
2026-04-09 04:01:05 +0800

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.post("/chat", async (req, res) => {
         console.log("Pesan masuk:", userMessage);
 
         const response = await fetch(
-            "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" + API_KEY,
+            "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-lite:generateContent?key=" + API_KEY,
             {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
